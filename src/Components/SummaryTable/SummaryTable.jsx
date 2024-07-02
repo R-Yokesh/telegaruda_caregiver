3090eb40d6f63345d9bfd79e99dcc5233456e763
Boobalan10 <redantmedia.boobalan@gmail.com>
2024-07-02 19:31:55 +0530

    patient summary screen vitals tab

---
+++ src/Components/SummaryTable/SummaryTable.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { CTable } from '@coreui/react'
+
+function SummaryTable({ columns, items, green, head }) {
+
+    return (
+        <div className={`table-sec ${green}`} >
+            <h4>{head}</h4>
+            <CTable columns={columns} items={items} tableHeadProps={{ color: 'dark' }} />
+        </div >
+    )
+}
+
+export default SummaryTable
